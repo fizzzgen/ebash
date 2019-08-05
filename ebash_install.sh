eb121e664446d92253f88314fa9ef4e680910d61
fizzzgen <40318860+fizzzgen@users.noreply.github.com>
2019-08-05 20:40:04 +0300

    Added new features
    record script is not implemented yet

--- ebash_install.sh
+++ ebash_install.sh
@@ -1,9 +1,19 @@
+#!/bin/bash -i
+
+rm -R ~/.ebash
+mkdir ~/.ebash
+mkdir ~/.ebash/scripts
+
 echo "using sudo for installing ripgrep"
 sudo apt-get install ripgrep
-echo "copy ebash to ~/.ebash"
-cp ebash ~/.ebash
-echo "adding alias to ~/.ebash"
-echo  "alias ebash='bash ~/.ebash'" >> ~/.bashrc
+echo "ebash files placed in ~/.ebash/"
+cp ebash ~/.ebash/ebash
+cp record_script.py ~/.ebash/record_script.py
+
+echo "adding alias to ~/.ebash/ebash"
+
+echo  "alias ebash='history -w && bash ~/.ebash/ebash'" >> ~/.bashrc
+
 echo "Success"
 echo "U need to restart teminal or to type 'source ~/.bashrc'"
 echo "Then try to type 'ebash --help'"
---
+++ record_script.py
@@ -0,0 +1,2 @@
+import os
+print("finding script in bash history")
